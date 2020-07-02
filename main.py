4a9e8a55c24a1ae11c834b3589fa4dc17b00edfe
Pedro Neto <pedroneto12092000@hotmail.com>
2020-07-01 22:53:03 -0300

    Added voting function

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import discord
+from vote import Vote
 
-TOKEN = 'NzI3MzA4Mjk2MDA4MjM3MjE4.Xvp9WQ.5M1fnSOF465U0SfAa5_rOwTZjc8'
+TOKEN = 'yourtokenhere'
 
 client = discord.Client()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
 
 @client.event
 async def on_ready():
@@ -31,9 +23,40 @@ async def on_member_join(member):
 
     embed.set_image(url = 'https://media.tenor.com/images/34a26802bf0a04f190b4195ed1f9b0a8/tenor.gif')
     embed.add_field(name = 'Gente nova entrando no Mangue :)', value=f'{boasvindas}\n\n')
-    channel = client.get_channel(575381401814302720)
 
     await member.guild.text_channels[0].send(embed = embed)
-    await member.setVoiceChannel(channel)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('-vote') and '(' in message.content and ')' in message.content:
+            question = message.content[message.content.find('(')+1:message.content.find(')')]
+            options = message.content[message.content.find(')')+2:].split()
+            
+            if len(options) <= 1:
+                await message.channel.send('Você precisa dar mais de 1 opção.')
+                return
+
+            if len(options) > 10:
+                await message.channel.send('Não é possivel fazer uma votação com mais de 10 opções.')
+                return
+
+            if len(options) == 2 and options[0] == 'sim' and options[1] == 'não':
+                reactions = ['✅', '❌']
+            else:
+                reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+
+            description = []
+            for x, option in enumerate(options):
+                description += '\n {} {}'.format(reactions[x], option)
+            embed = discord.Embed(title=question, description=''.join(description))
+            react_message = await message.channel.send(embed=embed)
+            for reaction in reactions[:len(options)]:
+                await react_message.add_reaction(reaction)
+
+    elif message.content.startswith('-vote'):
+        await message.channel.send('Digite um tópico entre parêntesis, exemplo:\n -vote (tópico aqui) opção1 opção2 opção3')
 
 client.run(TOKEN)
